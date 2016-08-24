206c859ace1b00f47a15b8c4e184b78e054172e8
DarshanMurthy <darshants.darshan1@gmail.com>
2016-08-23 23:17:32 -0700

    practice5

--- practice3.cpp
+++ practice3.cpp
@@ -1,25 +1,24 @@
 #include<iostream>
-#include<stack>
 #include<vector>
 
+
 using namespace std;
+
 
 int main(){
 
 
-	vector <int> vtr(10);
+	vector<int> vtr(10);
+	cout<< vtr.size()<<endl;
 
-	cout<< vtr.size();
 
-	for(int i =0 ;i<10;i++){
-		vtr.push_back(i);
-		cout<<vtr[i]<<endl;
+	for(int i=0;i<vtr.size();i++){
+		vtr[i]= i*i;
+		cout<< vtr[i] <<endl;
 	}
 
 
-
-
-
+	
 
 
 
---
+++ practice5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+int main(){
+
+	vector<string> vtr(10);
+
+	cout<< vtr.size()<<endl;
+
+
+
+
+}
