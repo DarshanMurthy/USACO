f0359eeb969e415a94d48c7817631ee72f30922c
DarshanMurthy <darshants.darshan1@gmail.com>
2016-08-24 07:40:39 -0700

    algorithms

--- practice5.cpp
+++ practice5.cpp
@@ -4,11 +4,24 @@
 using namespace std;
 
 
+int variable(&value){
+	cout<<value <<endl;
+}
+
+
 int main(){
 
 	vector<string> vtr(10);
 
 	cout<< vtr.size()<<endl;
+
+	int * ptr;
+
+
+cout<< ptr << endl;
+
+variable(ptr);
+
 
 
 
