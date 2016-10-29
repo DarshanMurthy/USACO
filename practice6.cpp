6b310f88285fb5d6c82de520df060822ff4d73e7
DarshanMurthy <darshants.darshan1@gmail.com>
2016-10-29 09:22:23 -0700

    add the malloc

--- practice6.cpp
+++ practice6.cpp
@@ -1,23 +1,39 @@
 #include<iostream>
-#include<vector>
+#include<stdlib.h>
+
 
 struct Node{
 	int data;
 	struct Node * llink;
-	struct Node * rrlink;
+	struct Node * rlink;
 };
 
 using namespace std;
 
+Node * creator(){
+
+	Node * p = (Node * )malloc(sizeof(Node));
+
+	return p;
+}
+
+
+
+
 int main(){
-	vector<int> vtr(100);
-	cout << vtr.size();
+	Node * ptr= creator();
+	ptr-> data =10;
+	cout<< ptr-> data;
+	cout<<"\n";
 
-	Node * node;
-	node-> data =100;
 
-	cout<< node-> data;
+	Node * ptr1 =  creator();
+	ptr1->data =100;
+	cout<< ptr1->data;
+
 
 
 	return 0;
+
+
 }
