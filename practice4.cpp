be1e692c904eb2b642f08a3ecb5d5da04d94979d
DarshanMurthy <darshants.darshan1@gmail.com>
2016-11-05 07:57:17 -0700

    added the node

--- practice4.cpp
+++ practice4.cpp
@@ -1,61 +1,12 @@
 #include<iostream>
-#include<vector>
-
-
-using namespace std;
-
-
-struct name{
-
-	int name;
-	char arr[100];
+struct Node{
+	int data;
+	struct Node * llink;
+	struct Node * rlink;
 
 };
 
-
 int main(){
-
-
-	vector <int> vtr(10);
-
-	cout<< vtr.size()<<endl;
-
-	for(int i=0;i<10;i++){
-		 vtr[i] = i*i;
-		 cout<< vtr[i]<<endl;
-	}
-
-
-	//nested
-	vector< vector <int> > vtr2(20);
-
-	
-
-
-	for(int i=9;i>0 ;i--){
-		vtr[i] -= vtr[i-1];
-		cout<<vtr[i]<<endl;
-	}
-
-
-	int sum=1;
-
-	for(int i=0;i<10;i++){
-		sum += i;
-	}
-
-	cout<< sum<<endl;
-
-
-	vector<int> v[10];
-
-	vector<int> v(10);
-
-
-
-
-
-
-
-	return 0;
+	struct Node * node;
+	node->data;
 }
